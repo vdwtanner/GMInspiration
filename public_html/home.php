7512c3bd5934eb5483e912e0e08f0c04f0ec245c
Zachary Corley <zdcorley@gmail.com>
2015-02-08 15:59:40 -0600

    index.html now automatically redirects to home.php, which displays our header! :D

---
+++ public_html/home.php
@@ -0,0 +1,31 @@
+<DOCTYPE html>
+<html>
+<head>
+	<title>Dungeon Crawlers</title>
+	<link rel="stylesheet" href="css/example/global.css" media="all">
+	<link rel="stylesheet" href="css/example/layout.css" media="all and (min-width: 33.236em)">
+	<?php include "header.php";?>
+</head>
+<body>
+<div id="container" class="cf">
+	<h3 style="text-align:center">The Dungeon Crawlers site is officially hosted! YAY!</h3>
+	<aside><div style="text-align: center">
+	<a href="contribute.php"><button style="border-radius: 10px;">Contribute!</button></a>
+	<a href="select_contribution_id.php"><button style="border-radius: 10px;">Test view a contribution</button></a>
+	<a href="profile.php"><button style="border-radius: 10px;">Test your Profile Page</button></a>
+	</div></aside>
+	<p>Now we just need to do the things:P</p>
+	<label for="Tanner updates">Tanner's updates to the group:</label>
+	<ul id="Tanner updates">
+		List of things Tanner did 1/23 before sleeps:
+		<li>set up server</li>
+		<li>set up database</li>
+		<li>set up user table</li>
+		<li>created this crappy page you are reading</li>
+		<li>created sign up page</li>
+		<li>created login page</li>
+		<li>did not sleep :P :P :P :P :P :P :P :P :P :P :P :P :P :P :P :P</li>
+	</ul>
+</div>
+</body>
+</html>
