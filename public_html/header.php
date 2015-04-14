011ac39c1f05c18ee568b3a3da6b4aa8f3a177c4
Zachary Corley <zdcorley@gmail.com>
2015-04-14 17:35:20 -0500

    fixed bold tag issue in header. Now the whole page doesnt render within bold tags

--- public_html/header.php
+++ public_html/header.php
@@ -205,10 +205,10 @@
 	
 	echo "<div style='clear: both;'>";
 	echo "<hr>";
-	echo "<b><a href='home.php'>Home</a>&nbsp;";
+	echo "<b><a href='home.php'>Home</a>&nbsp;</b>";
 	if($_SESSION["username"]){
-	echo "<b><a href='profile.php?user=".$_SESSION["username"]."'>Profile</a>&nbsp;";
-	echo "<b><a href='contribute.php'>Contribute</a>&nbsp;";
+	echo "<b><a href='profile.php?user=".$_SESSION["username"]."'>Profile</a>&nbsp;</b>";
+	echo "<b><a href='contribute.php'>Contribute</a>&nbsp;</b>";
 	}
 	echo "<form method='GET' action='search_results.php' style='display: inline;'>";
 	echo "<input type='text' name='keywords' style='width: 20em' placeholder='Enter keywords here' title='Search for users, contributions, types, subtypes, and game versions'>";
