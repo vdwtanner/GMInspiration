c5dfff85ae61da155587bb2fe5c4029d43d4309f
Dbarrett <dabarrett75@gmail.com>
2015-05-06 19:38:30 -0500

    the footer file

---
+++ public_html/footer.php
@@ -0,0 +1,37 @@
+<?php
+	session_start();
+	mb_internal_encoding("UTF-8");
+?>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+	
+</head>
+<body>
+
+<?php
+
+
+
+	echo "<div id='footcontainer'>";
+	
+
+	echo "<div style='clear: both;'>";
+	
+	echo "<a  id='homelink' href='index.html'><img src='img/title2.png'></a>";
+	
+	echo "</div>";
+	
+	echo "<div style='clear: both;'>";
+	echo "<hr>";
+	echo "<b><a class='hlink' href='home.php'>Home</a>&nbsp;</b>";
+	
+	echo "</div>";
+	echo "</div>";
+
+?>
+</body>
+</html>
+
